3d17306f0fd2b5f7d57376e1898800e1cf925840
rtopz <rtopz@rtopz-PC>
2013-09-08 20:47:39 -0700

    Added src folders

---
+++ src/com/example/scan2savetest/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.scan2savetest;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	public void startVoice(View view) {
+		Intent intent = new Intent(this, VoiceActivity.class);
+		startActivity(intent);
+	}
+
+	public void startPPG(View view) {
+		Intent intent = new Intent(this, PricePerGramActivity.class);
+		startActivity(intent);
+	}
+}
---
+++ src/com/example/scan2savetest/PricePerGramActivity.java
@@ -0,0 +1,52 @@
+package com.example.scan2savetest;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class PricePerGramActivity extends Activity {
+	Button calculateButton;
+	EditText priceEdit;
+	EditText weightEdit;
+	TextView ppg;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.activity_price_per_gram);
+
+		calculateButton = (Button)findViewById(R.id.calculateButton);
+		priceEdit   = (EditText)findViewById(R.id.priceEditText);
+		weightEdit   = (EditText)findViewById(R.id.weightEditText);
+		ppg = (TextView)findViewById(R.id.textView3);
+
+		 calculateButton.setOnClickListener(
+			        new View.OnClickListener()
+			        {
+			            public void onClick(View view)
+			            {
+			                calculatePricePerGram(view);
+			            }
+			        });
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.price_per_gram, menu);
+		return true;
+	}
+
+	public void calculatePricePerGram(View view){
+		double price = Double.parseDouble(priceEdit.getText().toString());
+		double weight = Double.parseDouble(weightEdit.getText().toString());
+		String pricePer100G = String.valueOf((price/weight)*100);
+		ppg.setText(pricePer100G);
+	}
+
+}
